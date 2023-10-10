3d59642fb6672182b2bb1413c80a42559dfbda61
Taku345 <takuma432@gmail.com>
2023-10-10 13:04:40 +0900

    my-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from "react";
+
+const URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/"
+const SHIGA = 250000;
+const KYOTO = 260000;
 
 function App() {
+  const [ state, setState ] = useState("");
+
+  useEffect(() => {
+    const getWeather = async () => {
+      const res = await axios.get(`${URL}${KYOTO}.json`);
+      setState(res.data);
+    }
+    getWeather();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>ウェザーリポート</h1>
+    <h2>京都市</h2>
+    {console.log(state)}
+    {
+      state[0]?.timeSeries[0].areas.map((area)=> {
+        return (
+          <>
+            <h3>---{area.area.name}---</h3>
+            {area.weathers.map((weather)=><p>{weather}</p>)}
+          </>
+        )
+      })
+    }
     </div>
   );
 }
